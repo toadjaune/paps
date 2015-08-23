1c02685ad96fd659d066b9dd67b1f604c686977a
Arnaud Venturi <arnaud.venturi@rez-gif.supelec.fr>
2015-08-23 23:49:48 +0200

    La base des bars fonctionne

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,10 +1,32 @@
 class BarsController < ApplicationController
+
+  def index
+    @bars = Bar.all
+  end
+
+  def edit
+    @bar = Bar.find(params[:id])
+  end
   
+  def show
+    @bar = Bar.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:bar].inspect
+    @bar = Bar.new(bar_params)
+    if @bar.save 
+      redirect_to @bar
+    else
+      render 'new'
+    end
   end
+
+  private
+    def bar_params
+      params.require(:bar).permit(:name)
+    end
 
 end
--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,2 +1,5 @@
 class Bar < ActiveRecord::Base
+
+  validates :name, presence: true,
+                   length: {minimum: 5 }
 end
