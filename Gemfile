54bb64e651a93be780b4b9dbd35e5084461f56a7
Arnaud Venturi <arnaud.venturi@rez-gif.supelec.fr>
2015-09-11 16:37:15 +0200

    Devise fonctionne (en fonctionnement basique) maitenant faut faire du nettoyage

--- Gemfile
+++ Gemfile
@@ -2,7 +2,9 @@ source 'https://rubygems.org'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.2.3'
+gem 'rails', '~> 4.2.3'
+
+gem 'devise', '~> 3.5.2'
 
 gem 'mysql2'
 # Use SCSS for stylesheets
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable #,:confirmable
   validates :name,  presence: true,
                     uniqueness: true
   validates :admin, inclusion: { in: [true, false] }
