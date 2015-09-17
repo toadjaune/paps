76aff10818a4cd0d398d4383ea4657f895198b7e
Arnaud Venturi <arnaud.venturi@rez-gif.supelec.fr>
2015-09-17 11:13:41 +0200

    Remis les validations et l'auto non-admin sur les nouveaux utilisateurs. Il y a un 'p bleh' qui traîne mais ça marche pas sans ...

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
 
-#  before_validation(on: :create) {self.admin = false} #:put_admin_to_false
+  before_validation(on: :create) do
+    self.admin = false
+    p "bleh"
+  end
   belongs_to :bar
   has_many :tentatives
 
@@ -9,7 +12,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable #,:confirmable
 
-#  validates :admin, inclusion: { in: [true, false] }
+  validates :admin, inclusion: { in: [true, false] }
 
 #  with_options unless: :admin do |n1a|
 #    n1a.validates :bar, presence: true
@@ -19,15 +22,5 @@ class User < ActiveRecord::Base
   def to_s
     email
   end
-
-  def authenticate!
-
-
-  end
-
-  private
-    def put_admin_to_false
-      self.admin = false
-    end
 
 end
