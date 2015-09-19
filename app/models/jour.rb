d7f2e2420a2bc65254becc5c8cfa4eb506ad8ef6
Arnaud Venturi <arnaud.venturi@rez-gif.supelec.fr>
2015-09-19 18:08:14 +0200

    Validation de l'unicité du jour actif

--- app/models/jour.rb
+++ app/models/jour.rb
@@ -7,10 +7,22 @@ class Jour < ActiveRecord::Base
   validates :max_affprev, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :max_commandos, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :actif, inclusion: { in: [true, false] }
+  
+  # Au plus un jour actif
+  validate do |jour|
+    if jour.actif? and (not (jour.id and Jour.find(jour.id).actif?)) and (Jour.where(actif: true).count >= 1)
+      jour.errors[:base] << "Il doit y avoir au plus une année courante"
+    end
+  end
+
+  def self.actif
+    Jour.where(actif: true).last
+  end
 
   def bars_restants
     Bar.all.reject{ |bar| bars.include?(bar) }
   end
+
   def to_s
     date
   end
