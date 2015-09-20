6c3005837eccca5d272f4fa85d5f91943e2ae833
Arnaud Venturi <arnaud.venturi@rez-gif.supelec.fr>
2015-09-20 15:58:01 +0200

    Supprimé l'ajout automatique de @supelec.fr, qui faisait des trucs ... bizarres.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,6 @@ class User < ActiveRecord::Base
   validates :email, format: { with: /.*@supelec\.fr/, message: 'L\'email doit être en @supelec.fr' }
   before_validation(on: :create) do
     self.admin = false
-    self.email = "#{self.email}@supelec.fr"
     p "bleh"
   end
   with_options unless: :admin do |n1a|
